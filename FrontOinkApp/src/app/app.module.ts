47f544cb763f4b1f50e6b31926685a17c8f8c02a
Carlos Villamizar <camaviga11@gmail.com>
2021-12-30 22:39:30 -0500

    Se agregó el componente para Mi cuenta donde va a estar el core de la aplicacion

--- FrontOinkApp/src/app/app.module.ts
+++ FrontOinkApp/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { InicioComponent } from './inicio/inicio.component';
+import { MiCuentaComponent } from './mi-cuenta/mi-cuenta.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    InicioComponent
+    InicioComponent,
+    MiCuentaComponent
   ],
   imports: [
     BrowserModule,
